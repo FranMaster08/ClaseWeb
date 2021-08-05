3cf8dfaec1ef75fde0eb8180ff121aa8daef20e8
Francisco Jimenez <francisco.jimenez@naranjax.com>
2021-08-04 22:30:20 -0300

    add: se crearon varias cosas

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+app.use(express.static(path.join(__dirname, "./public")));
 app.get("/", (req, res) => {
-  res.send("Hola Mundo");
+  res.sendFile(path.join(__dirname, "./view/index.html"));
 });
 
 app.listen(3000, () => {
